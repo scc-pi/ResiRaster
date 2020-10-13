7ac31888404232a1a5d5ca0d822b24a2912dd6dc
laurie-platt <laurieplatt@hotmail.com>
2020-10-13 08:05:29 +0100

    Moved ToDo's from script to GitHub Issues

--- ResiRaster.R
+++ ResiRaster.R
@@ -3,8 +3,6 @@ library(tidyverse); library(sf); library(raster)
 # Set variables
 geodb = "ResiRaster.gdb"
 
-# TODO: Use LLPG on Portal or update local LLPG copy
-
 # Read LLPG and filter out non residential
 resi <- st_read(dsn = geodb, layer = "llpg") %>% 
   filter(primaryclassification == "Residential") %>% 
@@ -14,8 +12,6 @@ resi <- st_read(dsn = geodb, layer = "llpg") %>%
 # Cast from multi-point to point
 resi <- st_cast(resi, "POINT")
 
-# TODO: iterate through array of resolution sizes - template & file name?
-
 # Create a raster object to put the residential points in
 resolution_size = 25
 raster_template = raster(extent(resi), 
@@ -24,8 +20,6 @@ raster_template = raster(extent(resi),
 
 # Convert the residential points (vector) to raster
 resi_raster = rasterize(resi, raster_template, field = 1)
-
-# TODO: arc.write to write to geodatabase or write to Portal?
 
 # Write raster to a file
 writeRaster(resi_raster, str_c("resi_raster_res", resolution_size), overwrite=TRUE)
